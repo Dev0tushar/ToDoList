81ceb6cbdc4582353f8534be3e9be22383ec8ef1
Dev0tushar <tsharma13791@gmail.com>
2023-02-23 19:25:09 +0530

    netlify error

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 const ToDo = ({ todo, handleToggle, handleChange }) => {
     const [edit, setEdit] = useState(false)
